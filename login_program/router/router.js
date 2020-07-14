c177a6da30a888947ff4275d99a4ae39fc4e9329
padamu1 <padamu1@naver.com>
2020-07-14 10:43:34 +0900

    first_working

--- login_program/router/router.js
+++ login_program/router/router.js
@@ -7,8 +7,57 @@ module.exports = function(app,fs)
 			username : sess.username
 		})
 	});
-	app.get('/login/:username/:password',function(req,res){
-		
+	app.post('/login', function(req, res){ //login function
+        	var sess; //session variable
+        	sess = req.session; //initial session
+
+        	fs.readFile(__dirname + "/../data/data.json", "utf8", function(err, data){ // 
+        		var users = JSON.parse(data);
+	        	var username = req.body.username;
+	        	var password = req.body.password;
+			console.log(username);
+			console.log(password);
+			
+	        	var result = {};
+	        	if(!users[username]){ //users[username] == null
+	        	    // USERNAME NOT FOUND
+			        result["success"] = 0;
+			        result["error"] = "not found";
+				console.log("data not exist");
+        		        //res.json(result);
+				res.redirect('/'); //move page
+        		        return;
+     			}
+	
+                	if(users[username]["password"] == password){
+                		result["success"] = 1;
+                		sess.username = username;
+                		sess.name = users[username]["name"];
+				console.log("password incorrect");
+                		//res.json(result);
+				res.redirect('/');
+	
+                	}else{
+                		result["success"] = 0;
+                		result["error"] = "incorrect";
+                		//res.json(result);
+				res.redirect('/');
+                	}	
+        	})	
 	});
+        app.post('/logout', function(req, res){
+    	        sess = req.session;
+    	        if(sess.username){
+                        req.session.destroy(function(err){
+                        if(err){
+                                console.log(err);
+                        }else{
+                                res.redirect('/');
+                	}
+            	})
+        	}else{
+        	        res.redirect('/');
+        	}
+        })
 	
 }
