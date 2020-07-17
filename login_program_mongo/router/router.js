484d3af51a8eedb11dbfc73dc7a589be129ad56a
padamu1 <padamu1@naver.com>
2020-07-17 11:55:31 +0900

    new patch

--- login_program_mongo/router/router.js
+++ login_program_mongo/router/router.js
@@ -9,21 +9,18 @@ module.exports = function(app,Userinfo)
 	});
 	app.post('/login', function(req, res){ //login function
         	var sess; //session variable
-        	sess = req.session; //initial session
-		var username = req.body.username;
-	        var password = req.body.password;
+        	sess = req.session; //initial session		
 		if(sess.logincheck==0) sess.logincheck = 1;
-		if((username != '')&&(password != '')){
+		if((req.body.username != '')&&(req.body.password != '')){
 			Userinfo.findOne({"userid":req.body.username},function(err,user){
 				if(!user) {
 					console.log("not exist");
 					sess.logincheck =  0;
-					res.redirect('/');
-					
+					res.redirect('/');					
 					}
 				else {
-					if(user.userpw == password) {
-						sess.username = username;
+					if(user.userpw == req.body.password) {
+						sess.username = req.body.username;
 						console.log("correct");
 						res.redirect('/');
 					}else{
@@ -31,8 +28,6 @@ module.exports = function(app,Userinfo)
 						res.redirect('/');
 					}
 				}
-			
-
 			});
         	}else res.redirect('/');
 	});
@@ -61,40 +56,45 @@ module.exports = function(app,Userinfo)
 		var sess; //session variable
         	sess = req.session;
 		sess.registcheck = 0;
-		if(sess.logincheck==0) sess.logincheck = 1;
-		var username = req.body.username;
-		var password = req.body.password;
-		var useremail = req.body.useremail;
-		if((username != '')&&(password != '')&&(useremail != '')){
-			Userinfo.find({"userid":req.body.username},function(err,user){
-				if(user.length!=0){
-					console.log("user already exist");
-					
-					res.render('regist',{
-						registcheck : sess.registcheck	
-					});
-				}else{
-					var users = new Userinfo();
-					users.userid = username;
-					users.userpw = password;
-					users.useremail = useremail;
-					users.save(function(err){
-						if(err){
-							console.error(err);
-							res.json({result:0});
-							return;
+		if(sess.logincheck!=1) sess.logincheck = 1;
+		if((req.body.username != '')&&(req.body.password != '')&&(req.body.useremail != '')){
+			if(req.body.password == req.body.password_check){
+				Userinfo.find({"userid":req.body.username},function(err,user){
+					if(user.length!=0){
+						console.log("user already exist");
+						
+						res.render('regist',{
+							registcheck : sess.registcheck	
+						});
+					}else{
+						var users = new Userinfo();
+						users.userid = req.body.username;
+						users.userpw = req.body.password;
+						users.useremail = req.body.useremail;
+						users.save(function(err){
+							if(err){
+								console.error(err);
+								res.json({result:0});
+								return;
+								}
+							else{
+								
+								console.log("submit");
+								console.log(users);
 							}
-						else{
-							
-							console.log("submit");
-							console.log(users);
-						}
-					});
-					sess.registcheck =1;
-					res.redirect('/');		
-				}
+						});
+						sess.registcheck =1;
+						res.redirect('/');
+						return;		
+					}
 				
-			});
+				});
+			}else{
+				sess.registcheck = 2;
+				res.render('regist',{
+					registcheck : sess.registcheck
+				});
+			}
 		}else{
 			res.render('regist',{
 				registcheck : sess.registcheck	
