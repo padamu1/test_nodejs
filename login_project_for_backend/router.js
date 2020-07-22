f79787e9bace4dbf25d2fb16b012a232189d04d3
padamu1 <padamu1@naver.com>
2020-07-22 15:34:16 +0900

    sample project

--- login_project_for_backend/router.js
+++ login_project_for_backend/router.js
@@ -4,21 +4,34 @@ const { userInfo } = require("os");
 router.post("/login",function(req,res) {
     if((req.body.username != '')&&(req.body.password != '')){
         UserInfo.findOne({"userid":req.body.username},function(err,user){
+            if(err){
+                throw res.json(err);
+            }
             if(!user){
-                return res.json({"success":"not user"})
+                throw res.json({"success":"not user"})
             }else{
                 if(user.userpw == req.body.password){
                     return res.json({"success":"user login"})
                 }else{
-                    return res.json({"success":"password incorrect"})
+                    throw res.json({"success":"password incorrect"})
                 }
             }
         })
     }else{
-        return res.json({"success":"fill all item"})
+        throw res.json({"success":"fill all item"})
     }
 })
 
-router.post('logout',function(req,res){
+router.post('/logout',function(req,res){
     return res.json({"success":"logout success"})
+})
+
+router.post('/register',function(req,res){
+    if((req.body.username != '')&&(req.body.password != '')&&(req.body.useremail != '')){
+        if(req.body.password == req.body.password_check){
+            Userinfo.find({"userid":req.body.username},function(err,user){
+
+            })
+        }
+    }
 })
